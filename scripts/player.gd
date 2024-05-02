2abcd61caca6ddc5650a10534595b477f24b903a
Hızır Ceylan <221307025@kocaeli.edu.tr>
2024-05-02 15:20:21 +0300

    Adding new animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -17,9 +20,26 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction : -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
+	
+	#Flip sprite
+	if direction > 0 :
+		animated_sprite_2d.flip_h = false
+	elif direction < 0 :
+		animated_sprite_2d.flip_h = true
+	
+	#Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")	
+	else:
+		animated_sprite_2d.play("jump")		
+
+	
+	# Apply movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
