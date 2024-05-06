c49864145899a384262b863aa562ea45608cf311
Hızır Ceylan <221307025@kocaeli.edu.tr>
2024-05-06 15:40:21 +0300

    Adding jumping SFX

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,7 @@ const JUMP_VELOCITY = -300.0
 
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -20,6 +21,7 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		animation_player.play("jump")
 
 	# Get the input direction : -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
